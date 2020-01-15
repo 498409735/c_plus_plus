ec9cb6444aa9362bc7f4eb090827545ca151c194
baobao <498409735@qq.com>
2020-01-15 13:48:12 +0800

    单例恶汉模式

--- single_hungry/single/main.cpp
+++ single_hungry/single/main.cpp
@@ -2,8 +2,39 @@
 
 using namespace std;
 
+class  Singleton
+{
+public:
+     static Singleton &getInstance() {
+        static Singleton instance;
+        return instance;
+     }
+     ~Singleton(){
+         total--;
+         cout<<"xigou total ="<<total<<endl;
+     }
+
+private:
+     Singleton(){
+         total++;
+         cout<<"gouzao total="<<total<<endl;
+     }
+     Singleton(const Singleton&other){
+         total++;
+         cout<<"copy total="<<endl;
+     }
+     static int total;
+};
+int Singleton::total = 0;
 int main()
 {
-    cout << "Hello World!" << endl;
+    Singleton ref;
+    cout<<"ref ="<<&ref<<endl;
+    Singleton &ref1 = Singleton::getInstance();
+    Singleton &ref2 = Singleton::getInstance();
+    Singleton &ref3 = Singleton::getInstance();
+    cout<<"&ref1 ="<<&ref1<<endl;
+    cout<<"&ref2 ="<<&ref2<<endl;
+    cout<<"&ref3 ="<<&ref3<<endl;
     return 0;
 }
--- test/test/main.cpp
+++ test/test/main.cpp
@@ -1,43 +1,75 @@
 #include <iostream>
 
 using namespace std;
-class Boat;
-class Car{
+class Car
+{
 public:
-    Car(const string name_tmp,const int weight_tmp)
-        :name(name_tmp),weight(weight_tmp){
-        cout<<"car initial"<<endl;
+    Car(const int wheel_tmp = 4,const string brand_tmp ="maX",const int price_tmp =10)
+        :wheel(wheel_tmp),brand(brand_tmp),price(price_tmp){
+        cout<<"new car added"<<endl;
     }
     ~Car(){
         cout<<"car delete"<<endl;
     }
-    friend int totalWeight(Car* car_tmp,Boat*boat_tmp);
-private:
-    string name;
-    int weight;
-};
-class Boat{
-public:
-    Boat(const string name_tmp,const int weight_tmp)
-        :name(name_tmp),weight(weight_tmp){
-        cout<<"Boat initial"<<endl;
+    int getwheel()const{
+        return this->wheel;
     }
-    ~Boat(){
-        cout<<"Boat delete"<<endl;
+    void setprice(const int price_tmp = 10 ){
+        this->price = price_tmp;
     }
-    friend int totalWeight(Car* car_tmp,Boat*boat_tmp);
+    inline void print(){
+        cout<<"wheel="<<this->wheel<<" brand= "<<this->brand<<" price="<<this->price<<endl;
+    }
 private:
-    string name;
-    int weight;
+    int wheel;
+    string brand;
+    int price;
 };
-int totalWeight(Car* car_tmp,Boat*boat_tmp){
-    int total = car_tmp->weight+boat_tmp->weight;
-    return  total;
-}
-int main()
-{
-    Car zs("zs",100);
-    Boat ls("ls",101);
-    cout<<totalWeight(&zs,&ls)<<endl;
+int main(){
+    Car sanlun(3,"feiniao",30);
+    cout<<"new car wheel ="<<sanlun.getwheel()<<endl;
+    sanlun.print();
+    sanlun.setprice(12);
+    sanlun.print();
     return 0;
 }
+//class Boat;
+//class Car{
+//public:
+//    Car(const string name_tmp,const int weight_tmp)
+//        :name(name_tmp),weight(weight_tmp){
+//        cout<<"car initial"<<endl;
+//    }
+//    ~Car(){
+//        cout<<"car delete"<<endl;
+//    }
+//    friend int totalWeight(Car* car_tmp,Boat*boat_tmp);
+//private:
+//    string name;
+//    int weight;
+//};
+//class Boat{
+//public:
+//    Boat(const string name_tmp,const int weight_tmp)
+//        :name(name_tmp),weight(weight_tmp){
+//        cout<<"Boat initial"<<endl;
+//    }
+//    ~Boat(){
+//        cout<<"Boat delete"<<endl;
+//    }
+//    friend int totalWeight(Car* car_tmp,Boat*boat_tmp);
+//private:
+//    string name;
+//    int weight;
+//};
+//int totalWeight(Car* car_tmp,Boat*boat_tmp){
+//    int total = car_tmp->weight+boat_tmp->weight;
+//    return  total;
+//}
+//int main()
+//{
+//    Car zs("zs",100);
+//    Boat ls("ls",101);
+//    cout<<totalWeight(&zs,&ls)<<endl;
+//    return 0;
+//}
